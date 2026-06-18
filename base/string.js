ad1c2b6243146c5011a34fe1a8c258a989f45f8d
abhijeet-guptaa <abhijeetdln88@gmail.com>
2026-06-18 12:29:30 +0000

    config

---
+++ base/string.js
@@ -0,0 +1,9 @@
+//const name = "abhijeet"
+//const repoCount = 50
+
+//console.log(`hellow my namne is ${name} and my repo count is ${repoCount}`)
+
+const newStringOne = "  abhijeet  "
+console.log(newStringOne);
+console.log(newStringOne.trim ());
+
