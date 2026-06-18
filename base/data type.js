befebfada12e14794eb9bfcc5ff6963844a86358
abhijeet-guptaa <abhijeetdln88@gmail.com>
2026-06-18 02:50:32 +0000

    config

---
+++ base/data type.js
@@ -0,0 +1,5 @@
+//kis tarh memory me dat ko rakha jaye ya accese kiya jaye
+//usko data type kahte hai......
+//two type of dat premitive and refernce type
+// 7 type premitive 1 string,number,bollean,null,undefine,bigint,symbol
+// reference [arrays,object,function]
