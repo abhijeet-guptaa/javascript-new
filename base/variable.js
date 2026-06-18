1dfbf6cad4982d669b50906662f2786c3962c918
abhijeet-guptaa <abhijeetdln88@gmail.com>
2026-06-18 02:39:58 +0000

    config

---
+++ base/variable.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "abhi@122"
+var accountPassword ="12nnmqw34"
+
+//accountID = 12// not allowed
+accountEmail = "abhi@12"
+accountPassword = 2333
+accountCity = "delhi"
+
+console.log("accountEmail")
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
